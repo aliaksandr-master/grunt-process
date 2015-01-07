c7ebc25d8d34cd83d3f74331207de0831138710c
Aliaksandr Pasynkau <alxe.master@gmail.com>
2015-01-07 22:36:56 +0300

    add tests

---
+++ tests/compare.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var path = require('path');
+var grunt = require('grunt');
+
+module.exports['must be equal with examples'] = function (test) {
+	var fs = grunt.file.expand({cwd: '.tmp', filter: 'isFile'}, ['**/*.{json,txt}']);
+
+	fs.forEach(function (f) {
+		var result = grunt.file.read(path.join('.tmp', f));
+		var expect = grunt.file.read(path.join('tests/expect', f));
+
+		test.equal(result, expect);
+	});
+
+	test.done();
+};
