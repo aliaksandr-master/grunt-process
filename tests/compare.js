0f227b645d285d7db4336469fc448e9e4ec8ff70
aliaksandr-pasynkau <alxe.master@gmail.com>
2015-05-05 19:09:18 +0300

    update tests

--- tests/compare.js
+++ tests/compare.js
@@ -1,10 +1,15 @@
-"use strict";
+'use strict';
 
 var path = require('path');
 var grunt = require('grunt');
 
 module.exports['must be equal with examples'] = function (test) {
-	var fs = grunt.file.expand({cwd: '.tmp', filter: 'isFile'}, ['**/*.{json,txt}']);
+	var fs = grunt.file.expand({
+		cwd: '.tmp',
+		filter: 'isFile'
+	}, [
+		'**/*.{json,txt}'
+	]);
 
 	fs.forEach(function (f) {
 		var result = grunt.file.read(path.join('.tmp', f));
--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,23 @@
-"use strict";
+'use strict';
 
+var grunt = require('grunt');
 var process = require('./_lib');
 
-exports['testing'] = function (test) {
-	test.ok(true);
-	test.done();
+exports['no files'] = function (test) {
+	process(grunt, [], {}, function (err) {
+		test.ok(!err);
+		test.done();
+	});
+};
+
+exports['empty files'] = function (test) {
+	process(grunt, [
+		{
+			src: [ 'tests/source/unit/process.txt' ],
+			dest: '.tmp/unit/process.txt'
+		}
+	], {}, function (err) {
+		test.ok(!err);
+		test.done();
+	});
 };
